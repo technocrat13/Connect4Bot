a882cc9322ca5c15e8face763ed4efd700fe3271
Ayush Bishnoi <ayushbishnoi@live.com>
2021-07-16 01:15:44 +0530

    adding pytest in actions

--- gameAIButBetter.py
+++ gameAIButBetter.py
@@ -6,7 +6,6 @@ import datetime
 import numpy as np
 
 
-
 y, x = 6, 7
 gameboard = [[0 for j in range(x)] for i in range(y)]
 
@@ -15,12 +14,8 @@ MOVES = []
 TOPPED_OUT = []
 NOT_TOPPED_OUT = [i for i in range(x)]
 
-#q_table = {}s
 print('loading shelve....')
 q_table = shelve.open('q_table_shelf.db',  writeback=True)
-
-#with open("qtable.txt", "rb") as myFile:
-#    q_table = pickle.load(myFile)
 
 
 def print_gameboard():
@@ -35,8 +30,6 @@ def check_connect_list(list_to_check):
 
     if len(list_to_check) != 4 or 0 in list_to_check:
         return False
-
-    #print(list_to_check)
 
     if sum(list_to_check) in [20, 28]:
         return True
@@ -55,24 +48,19 @@ def check_diagonals(x_pos, y_pos):
                 try:
                     list_diagonal_top.append(
                         gameboard[y_pos - 4 + i][x_pos - 4 + i])
-                    #list_diagonal_bottom.append(gameboard[y_pos + 4 - i][x_pos - 4 + i])
                 except IndexError:
                     index_error_count = index_error_count + 1
 
             if y_pos + 4 - i >= 0 and x_pos - 4 + i >= 0:
                 try:
-                    #list_diagonal_top.append(gameboard[y_pos - 4 + i][x_pos - 4 + i])
                     list_diagonal_bottom.append(
                         gameboard[y_pos + 4 - i][x_pos - 4 + i])
                 except IndexError:
                     index_error_count = index_error_count + 1
 
-        #print('checking diagonal top down ', end=' ')
-
         if check_connect_list(list_diagonal_top) is True:
             return '4connected'
 
-        #print('checking diagonal bottom up ', end=' ')
         if check_connect_list(list_diagonal_bottom) is True:
             return '4connected'
 
@@ -83,9 +71,7 @@ def check_connect_4(x_pos, y_pos):
     '''checks if there is a 4 sequence from the recived x and y coord'''
 
     index_error_count = 0
-    # checking x axis
 
-    #print('checking x axis ', end=' ')
     for i in range(1, 5):
         try:
             if check_connect_list(gameboard[y_pos][x_pos - 4 + i: x_pos + i]) is True:
@@ -93,67 +79,38 @@ def check_connect_4(x_pos, y_pos):
         except IndexError:
             index_error_count = index_error_count + 1
 
-    # checking bellow x_pos
-
-    #print('checking x to up ', end=' ')
     try:
         if check_connect_list([gameboard[i][x_pos] for i in range(y_pos - 0, y_pos + 4)]) is True:
             return '4connected'
     except IndexError:
         index_error_count = index_error_count + 1
-        #print('col not tall enough')
 
-    # checking diagonals
     return check_diagonals(x_pos, y_pos)
-
-    # check bottom up diagonals
-
-    #for j in range(0, 4):
-    #    list_diagonal = []
-    #    for i in range(1 + j, 5 + j):
-    #        try:
-    #            list_diagonal.append(gameboard[y_pos + 4 - i][x_pos - 4 + i])
-    #        except IndexError:
-    #            index_error_count = index_error_count + 1
-
-    #    if check_connect_list(list_diagonal) is True:
-    #        return '4connected'
 
 
 def probable_move(x_pos, y_pos):
     '''checks if coin placed at this position connects4'''
 
-
     for i in [7, 5]:
         gameboard[y_pos][x_pos] = i
-        #print('probablemove')
-        #print_gameboard()
+
         if check_connect_4(x_pos, y_pos) == '4connected':
             gameboard[y_pos][x_pos] = 0
-            #if i == 7:
-                #print('aha saved a connect4 at ' + str(x_pos + 1))
-            #else:
-                #print('i win in this simple move at ' + str(x_pos + 1))
             return '4connected'
         gameboard[y_pos][x_pos] = 0
 
     return False
 
+
 def keywithmaxval(dic):
-    """ a) create a list of the dict's keys and values;
-        b) return the key with the max value
-    https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary"""
     k = list(dic.keys())
-    # boltzmann
     val = np.array(list(dic.values()))
-    # If there are multiple max values, choose randomly
     return k[int(random.choice(np.argwhere(val == np.amax(val))))]
 
 
-# the percentage of time when we should take the best action (instead of a random action)
 EPSILION = 1
-DISCOUNT_FACTOR = 0.9  # discount factor for future rewards
-LEARNING_RATE = 0.9  # the rate at which the AI agent should learn
+DISCOUNT_FACTOR = 0.9
+LEARNING_RATE = 0.9
 
 
 def is_creating_4_above(x_pos):
@@ -165,16 +122,10 @@ def is_creating_4_above(x_pos):
 
     for i in [7, 5]:
         gameboard[y_pos - 1][x_pos] = i
-        #print('probablemove')
-        #print_gameboard()
-        #print('is this working hello')
-        #print_gameboard()
         if check_connect_4(x_pos, y_pos - 1) == '4connected':
-            #print('is this working hello')
             gameboard[y_pos - 1][x_pos] = 0
             return True
         gameboard[y_pos - 1][x_pos] = 0
-
 
     if check_connect_4(x_pos, top[x_pos] - 1) == '4connected':
         print('checking above xpos')
@@ -186,10 +137,6 @@ def is_creating_4_above(x_pos):
 def stop_4_connecting():
     '''check_connect_4(takes x_pos and then y_pos) to highest y_pos and check for every x_pos'''
 
-    #(0, top[0])
-    #print('top in stop4connecting: ', end='')
-    #print(top)
-
     if TURN == 0:
         random.choice([2, 3, 4])
 
@@ -198,7 +145,6 @@ def stop_4_connecting():
             return random.choice([2, 4])
         return 3
 
-
     length = len(top)
     for i in range(length):
         if top[i] == -1:
@@ -206,16 +152,14 @@ def stop_4_connecting():
         if probable_move(i, top[i]) == '4connected':
             if check_connect_4(i, top[i]) == '4connected':
                 return i
-            #good_move.append(i)
             return i
 
     move = random.choice(NOT_TOPPED_OUT)
-    gameboard_as_key = ''.join(str(item) for innerlist in gameboard for item in innerlist)
+    gameboard_as_key = ''.join(str(item)
+                               for innerlist in gameboard for item in innerlist)
     if np.random.random() < EPSILION:
         move = keywithmaxval(q_table[gameboard_as_key])
 
-    #print('NOTTOPPEDOUT in stop4connecting: ', end='')
-    #print(NOT_TOPPED_OUT)
     count = 0
     while is_creating_4_above(move) is True:
         if count == 5:
@@ -232,7 +176,7 @@ def valid_move(x_pos, y_pos):
 
     if x_pos in TOPPED_OUT:
         return False
-
+    #print(NOT_TOPPED_OUT)
     if y_pos == 0:
         TOPPED_OUT.append(x_pos)
         NOT_TOPPED_OUT.remove(x_pos)
@@ -262,11 +206,7 @@ def add_coin(x_pos):
             gameboard[j][x_pos] = COIN
             y_pos = j
             top[x_pos] = top[x_pos] - 1
-            #print('top in addcoin: ', end='')
-            #print(top)
             return check_connect_4(x_pos, y_pos)
-
-    return 'not valid'
 
 
 def add_coin_key_maker(x_pos):
@@ -278,7 +218,11 @@ def add_coin_key_maker(x_pos):
     gameboard[y_pos][x_pos] = COIN
     key = ''.join(str(item) for innerlist in gameboard for item in innerlist)
     gameboard[y_pos][x_pos] = 0
-    return key
+    actions_possible = NOT_TOPPED_OUT.copy()
+
+    if y_pos == 0:
+        actions_possible.remove(x_pos)
+    return key, actions_possible
 
 
 def remove_coin(x_pos):
@@ -296,17 +240,15 @@ def get_move_reward(x_pos):
     y_pos = top[x_pos]
     for i in [7, 5]:
         gameboard[y_pos][x_pos] = i
-        #print('calculating reward')
-        #print_gameboard()
 
         if check_connect_4(x_pos, y_pos) == '4connected':
             gameboard[y_pos][x_pos] = 0
             if i == 7:
-                print('aha saved a connect4 at ' + str(x_pos + 1))
+                #print('aha saved a connect4 at ' + str(x_pos + 1))
                 return -500
-            print('i win in this simple move at ' + str(x_pos + 1))
+            #print('i win in this simple move at ' + str(x_pos + 1))
             return 10000
-            #return '4connected'
+
         gameboard[y_pos][x_pos] = 0
 
     return -1
@@ -316,9 +258,7 @@ def generate_next_move(board_state):
     '''next move is generated either through random or highest q value, EPSILION mediates this'''
 
     gameboard_as_key = ''.join(str(item)
-                                for innerlist in board_state for item in innerlist)
-
-    #print(gameboard_as_key)
+                               for innerlist in board_state for item in innerlist)
 
     if gameboard_as_key not in q_table:
         q_table[gameboard_as_key] = {el: 0 for el in NOT_TOPPED_OUT}
@@ -329,16 +269,30 @@ def generate_next_move(board_state):
 
     old_q_value = q_table[gameboard_as_key][action]
 
-    gameboard_as_key_next = add_coin_key_maker(action)
-                    
+    #add_coin(action)
+    #add_coin_key_maker(action)
+
+    gameboard_as_key_next, next_moves = add_coin_key_maker(action)
+
+    #print('TURN: ' + str(TURN) + ' -> ', end=' ')
+    #print(next_moves)
+    #print_gameboard()
+
+    if next_moves == []:
+        next_moves = NOT_TOPPED_OUT
+
     if gameboard_as_key_next not in q_table:
-        q_table[gameboard_as_key_next] = {el: 0 for el in NOT_TOPPED_OUT}
+        q_table[gameboard_as_key_next] = {el: 0 for el in next_moves}
 
+    #if TURN <= 41:
 
+    #remove_coin(action)
+
+    #print_gameboard()
 
     temporal_difference = reward + \
         (DISCOUNT_FACTOR * q_table[gameboard_as_key]
-         [keywithmaxval(q_table[gameboard_as_key_next])]) - old_q_value
+         [keywithmaxval(q_table[gameboard_as_key_next].copy())]) - old_q_value
 
     interim_dict = q_table[gameboard_as_key]
     new_q_value = old_q_value + (LEARNING_RATE * temporal_difference)
@@ -366,8 +320,7 @@ def swap_gameboard():
 def take_input(player):
     '''takes input if users chance, otherwise generates ai_move'''
 
-
-    if player == 7: #change to 7 for 
+    if player == 7:  # change to 7 for
         COIN = 5
         #print_gameboard()
         swap_gameboard()
@@ -379,23 +332,29 @@ def take_input(player):
         return ai_2_move
 
 
-    if player == 9:
-        return random.choice(NOT_TOPPED_OUT)
+#    if player == 9:
+#        return random.choice(NOT_TOPPED_OUT)
+#
+#    if player == 8:
+#        move = input('drop COIN at: ').rstrip()
+#        while valid_move_x(move) is False:
+#            move = input('last input invalid, go again: ').rstrip()
+#        return int(move) - 1
 
-    if player == 8:
-        move = input('drop COIN at: ').rstrip()
-        while valid_move_x(move) is False:
-            move = input('last input invalid, go again: ').rstrip()
-        return int(move) - 1
-
-
-    print('AI is calculating its next move.....')
+    #print('AI is calculating its next move.....')
     #ai_move = stop_4_connecting()
     ai_move = generate_next_move(gameboard)
 
     #ai_move = random.randint(0, 6)
-    print('Playing at: ' + str(ai_move + 1))
+    #print('Playing at: ' + str(ai_move + 1))
     return ai_move
+
+
+def stamp_time():
+    later_time = datetime.datetime.now()
+    difference = later_time - first_time
+    print(str(divmod((difference.days * 24 * 60 * 60) +
+                     difference.seconds, 60)[0] / 60) + ' Hours gone by')
 
 
 EPISODES = 3
@@ -407,7 +366,7 @@ DRAW = False
 first_time = datetime.datetime.now()
 
 #print_gameboard()
-print('biginning best of ' + str(EPISODES))
+
 
 for e in range(EPISODES):
     print('---------------------------------------new game----------------------------------------')
@@ -447,7 +406,8 @@ for e in range(EPISODES):
             WINS_7 = WINS_7 + 1
         else:
             WINS_5 = WINS_5 + 1
-        print('wow ' + str(COIN) + ' wins game ' + str(e + 1) + '/' + str(EPISODES))
+        print('wow ' + str(COIN) + ' wins game ' +
+              str(e + 1) + '/' + str(EPISODES))
     else:
         DRAWS = DRAWS + 1
         DRAW = False
@@ -458,8 +418,6 @@ for e in range(EPISODES):
           str(WINS_7 * 100 / (e + 1)) + ' | draw%: ' + str(DRAWS * 100 / (e + 1)))
     print(str(((e + 1) / EPISODES) * 100) + '%' + ' completion')
 
-
-
     if (e + 1) % (EPISODES/100) == 0:
         print('quicksaving')
         q_table.sync()
@@ -467,7 +425,6 @@ for e in range(EPISODES):
         difference = later_time - first_time
         print(str(divmod((difference.days * 24 * 60 * 60) +
                          difference.seconds, 60)[0] / 60) + ' Hours gone by')
-
 
 
 q_table.close()
